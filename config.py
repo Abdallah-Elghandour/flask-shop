cee225ec704759fe30aafb391a88e4ae3cc5b77e
Abdallah-Elghandour <Abdallahelghanodur1@gmail.com>
2024-09-12 22:36:33 +0300

    update

---
+++ config.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+class Config:
+    MONGODB_URI = os.environ.get("MONGODB_URI")
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    
